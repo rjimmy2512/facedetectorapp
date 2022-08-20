7613e74ac6fe5853860ec162ff03b732ca0508e9
James Ruhashyankiko <jruhashyankiko@activated.io>
2022-08-19 19:07:43 -0700

    created and formatted react components of the app and pushed to react-components branch

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Face Detector App
-## This app uses face-api.js detection api to track one's face and predicts facial expression, age and gender.
+
+This app uses face-api.js detection api to track one's face and predicts facial expression, age and gender.
+
+## Starting the App
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
---
+++ src/App.js
@@ -0,0 +1,57 @@
+import * as faceapi from "face-api.js";
+import './styles.css';
+
+const video = document.getElementById('video')
+
+function App() {
+
+  Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+    faceapi.nets.ageGenderNet.loadFromUri('/models')
+  ]).then(startVideo)
+  
+  function startVideo() {
+    navigator.getUserMedia(
+      { video: {} },
+      stream => video.srcObject = stream,
+      err => console.error(err)
+    )
+  }
+  
+  video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+  
+    faceapi.matchDimensions(canvas, displaySize)
+  
+    setInterval(async () => {
+      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withAgeAndGender();
+  
+      const resizedDetections = faceapi.resizeResults(detections, displaySize)
+  
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+  
+      faceapi.draw.drawDetections(canvas, resizedDetections)
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+      
+      resizedDetections.forEach(result => {
+        const { age, gender, genderProbability } = result;
+        new faceapi.draw.DrawTextField(
+          [
+            `${faceapi.utils.round(age, 0)} years`,
+            `${gender} (${faceapi.utils.round(genderProbability)})`
+          ],
+          result.detection.box.bottomRight
+        ).draw(canvas);
+      })
+    }, 100)
+  })
+  
+}
+
+export default App;
